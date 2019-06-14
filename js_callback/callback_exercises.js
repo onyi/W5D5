c3e7b8b0956a102578c8c4f4f1b2827faa025781
App Academy Student <student@appacademy.io>
2019-06-14 14:14:49 -0700

    Complete addNumbers js

--- js_callback/callback_exercises.js
+++ js_callback/callback_exercises.js
@@ -1,38 +1,84 @@
-class Clock {
-  constructor() {
-    // 1. Create a Date object.
-    this.d = new Date();
-    // 2. Store the hours, minutes, and seconds.
-    this.updateTime();
-    // 3. Call printTime.
-    this.printTime();
-    // 4. Schedule the tick at 1 second intervals.  
-  }
+////////////////////////
+//  Clock
+////////////////////////
 
-  printTime() {
-    this.updateTime();
-    console.log(`${this.h}:${this.m}:${this.s}`);
-    // Format the time in HH:MM:SS
-    // Use console.log to print it.
-  }
+// class Clock {
+//   constructor() {
+//     // 1. Create a Date object.
+//     this.d = new Date();
+//     // 2. Store the hours, minutes, and seconds.
+//     this.updateTime();
+//     // 3. Call printTime.
+//     this.printTime();
+//     // 4. Schedule the tick at 1 second intervals.  
+//   }
 
-  _tick() {
-    // 1. Increment the time by one second.
-    let that = this;
-    setInterval(function(){
-      that.d = new Date();
-      that.printTime();
-    },1000)
-    // 2. Call printTime.
-  }
+//   printTime() {
+//     this.updateTime();
+//     console.log(`${this.h}:${this.m}:${this.s}`);
+//     // Format the time in HH:MM:SS
+//     // Use console.log to print it.
+//   }
 
-  updateTime(){
-    this.s = this.d.getSeconds();
-    this.m = this.d.getMinutes();
-    this.h = this.d.getHours();
+//   _tick() {
+//     // 1. Increment the time by one second.
+//     let that = this;
+//     setInterval(function(){
+//       that.d = new Date();
+//       that.printTime();
+//     },1000)
+//     // 2. Call printTime.
+//   }
+
+//   updateTime(){
+//     this.s = this.d.getSeconds();
+//     this.m = this.d.getMinutes();
+//     this.h = this.d.getHours();
+//   }
+// }
+
+// const clock = new Clock();
+// // clock.printTime();
+// clock._tick();
+
+
+
+
+
+
+////////////////////////
+//  addNumbers
+////////////////////////
+
+
+const readline = require("readline");
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// reader.question("Enter a number: \n", function(res1) {
+//   addNumbers(res1, , completionCallback);
+//   // reader.close();
+// });
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  console.log(`numsLeft: ${numsLeft}`);
+  if (numsLeft === 0) {
+    completionCallback(sum);
+    reader.close();
+  }
+  
+  if (numsLeft > 0) {
+    reader.question("Enter a number: \n", function(res2) {
+      let num = parseInt(res2);
+      sum += num;
+      console.log(`Current sum: ${sum}`);
+      addNumbers(sum, (numsLeft - 1), completionCallback);
+    });
   }
 }
 
-const clock = new Clock();
-// clock.printTime();
-clock._tick();
+addNumbers(0, 3, sum => console.log(`Total sum: ${sum}`));
+
+// console.log("\n\nlast line\n\n");
